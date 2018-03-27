bc142279bc7463d2cdc0c44a001fa2ec93b9f968
anupam-tavant <anupam.mohapatra@tavant.com>
2018-03-27 18:08:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Angular2TokenService} from "angular2-token";
+import {environment} from "../environments/environment";
+import { HttpModule } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private _authToken: Angular2TokenService){
+    this._authToken.init(environment.token_auth_config);
+/*     this._authToken.signIn({email: "user@example.com", password: "monkey67"}).subscribe(
+
+      res => {
+
+        console.log('auth response:', res);
+        console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
+        console.log('auth response body:', res.json()); //log the response body to show the user 
+      },
+
+      err => {
+        console.error('auth error:', err);
+      }
+  ) */
+  }
+  title = 'Home Title';
+  developer: String="Anupam Mohapatra";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { ProductComponent } from './product/product.component';
+import { RouterModule } from '@angular/router';
+import { ProductService } from './product/product.service';
+import { ProductDetailComponent } from './product/product-detail.component';
 
+import { MaterializeModule } from 'angular2-materialize';
+
+import { Angular2TokenService } from 'angular2-token';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    ToolbarComponent,
+    AuthDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    MaterializeModule,
+    RouterModule.forRoot([
+      {path: "products", component: ProductComponent},
+      {path: "dashboards", component: DashboardComponent},
+      {path: "", redirectTo: "dashboards", pathMatch: "full"},
+      {path: 'products/:id', component: ProductDetailComponent}
+    ])
   ],
-  providers: [],
+  providers: [ProductService,Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
